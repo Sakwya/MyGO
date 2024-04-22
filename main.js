d30badb837872e3acb7d276d4b0d586f86ca0b31
Lieatis Sakwya <celiasash@foxmail.com>
2024-04-22 15:53:13 +0800

    love<3

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-rimgs = document.querySelectorAll(".rimg-content")
-for (const rimg of rimgs) {
-	let children = rimg.children
-	if(children.length!=2)throw Error(".rimg-content should only have 2 children")
-	let width = children[0].clientWidth;
-	let height = children[0].clientHeight;
-	children[1].style.width = `calc( 80vw - 6rem - ${width}px )`
-	children[1].style.height = `${height}px`
+function fitReshape() {
+	const rimgs = document.querySelectorAll(".rimg-content")
+	const pageWidth = document.querySelector(".main-post").clientWidth
+	for (const rimg of rimgs) {
+		let children = rimg.children
+		children[1].style.width = 0
+		if (children.length != 2) throw Error(".rimg-content should only have 2 children")
+		let width = children[0].clientWidth;
+		let height = children[0].clientHeight;
+		children[1].style.width = `calc( ${pageWidth}px - 6rem - ${width}px )`
+		children[1].style.height = `${height}px`
+	}
 }
+fitReshape()
+window.addEventListener("resize",()=>{
+	fitReshape();
+})
